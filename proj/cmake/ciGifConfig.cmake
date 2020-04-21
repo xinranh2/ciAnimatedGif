42cbc728d9cc590cb0e2736d552dbc044669371d
cindia hua <xinranh2@illinois.edu>
2020-04-20 21:14:10 -0500

    corrected name of block to ciGif

--- proj/cmake/ciGifConfig.cmake
+++ proj/cmake/ciGifConfig.cmake
@@ -1,30 +1,30 @@
-if(NOT TARGET ciAnimatedGif)
-    # Define ${ciAnimatedGif_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
-    get_filename_component(ciAnimatedGif_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE)
+if(NOT TARGET ciGif)
+    # Define ${ciGif_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
+    get_filename_component(ciGif_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE)
 
     # Define ${CINDER_PATH} as usual.
     get_filename_component(CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE)
 
     # Make a list of source files and define that to be ${SOURCE_LIST}.
     file(GLOB SOURCE_LIST CONFIGURE_DEPENDS
-            "${ciAnimatedGif_SOURCE_PATH}/ciAnimatedGif.cpp"
-            "${ciAnimatedGif_SOURCE_PATH}/ciAnimatedGif.h"
+            "${ciGif_SOURCE_PATH}/ciAnimatedGif.cpp"
+            "${ciGif_SOURCE_PATH}/ciAnimatedGif.h"
             )
 
     # Create the library!
-    add_library(ciAnimatedGif ${SOURCE_LIST})
+    add_library(ciGif ${SOURCE_LIST})
 
     # Add include directories.
     # Notice that `cinderblock.xml` has `<includePath>src</includePath>`.
     # So you need to set `../../src/` to include.
-    target_include_directories(ciAnimatedGif PUBLIC "${ciAnimatedGif_SOURCE_PATH}/src" )
-    target_include_directories(ciAnimatedGif SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+    target_include_directories(ciGif PUBLIC "${ciGif_SOURCE_PATH}/src" )
+    target_include_directories(ciGif SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
 
 
     # If your Cinder block has no source code but instead pre-build libraries,
     # you can specify all of them here (uncomment the below line and adjust to your needs).
     # Make sure to use the libraries for the right platform.
-    # # target_link_libraries(ciAnimatedGif "${Cinder-OpenCV_PROJECT_ROOT}/lib/libopencv_core.a")
+    # # target_link_libraries(ciGif "${Cinder-OpenCV_PROJECT_ROOT}/lib/libopencv_core.a")
 
     if(NOT TARGET cinder)
         include("${CINDER_PATH}/proj/cmake/configure.cmake")
@@ -32,6 +32,6 @@ if(NOT TARGET ciAnimatedGif)
                 "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
                 "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
     endif()
-    target_link_libraries(ciAnimatedGif PRIVATE cinder)
+    target_link_libraries(ciGif PRIVATE cinder)
 
 endif()
